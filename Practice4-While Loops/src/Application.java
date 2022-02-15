06cfe0bebfd283b37bf1545359d40893a393175f
mohamed.jalloh <mohamed.jalloh@WEPLUS97.wenet.local>
2022-02-15 16:27:41 +0100

    While Loops

---
+++ Practice4-While Loops/src/Application.java
@@ -0,0 +1,17 @@
+
+public class Application {
+	public static void main(String[] args) {
+//		int value=10;
+//		boolean loop=value>20;
+//		System.out.println(loop);
+		int value=0;
+		while (value<10) {
+			
+			System.out.println("Hello" + value);
+			
+			value=value+1;
+			
+		}
+		
+}
+}
